d431214ae8527605ae3d4d233c2dfaa78d4ebdfc
quatricise <stepantrvaj@gmail.com>
2023-06-03 22:01:32 +0200

    Update loadFonts.js

--- js/loadFonts.js
+++ js/loadFonts.js
@@ -1,7 +1,7 @@
 async function loadFonts(callback) {
   let fonts = [
-    {family: "space",       url: "fonts/space_grotesk_variable.ttf"},
-    {family: "big-t-comic", url: "fonts/big-t-comic.ttf"},
+    {family: "space",       url: "./fonts/space_grotesk_variable.ttf"},
+    {family: "big-t-comic", url: "./fonts/big-t-comic.ttf"},
   ]
   for(let font of fonts) {
     let fontFace = new FontFace(font.family, `url('${font.url}')`, {style: "normal", weight: 400})
